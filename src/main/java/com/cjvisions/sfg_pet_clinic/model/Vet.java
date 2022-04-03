d29a32ecbb08c2ce110f562b28d98ceae7e540ea
emmanuelcudjoe <emmanuel.cudjoe@turntabl.io>
2022-04-03 11:40:46 +0000

    Added POJO's Closes #2

---
+++ src/main/java/com/cjvisions/sfg_pet_clinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.cjvisions.sfg_pet_clinic.model;
+
+public class Vet extends Person {
+}
---
+++ src/test/java/com/cjvisions/sfg_pet_clinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjvisions.sfg_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
