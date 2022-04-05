049a7422c97cb2f6c5c38160e67beaf7afe45aa2
emmanuelcudjoe <emmanuel.cudjoe@turntabl.io>
2022-04-05 08:43:17 +0000

    Added Index page and Controller. Closes #8

---
+++ pet_clinic_web/src/main/java/com.cjvisions.sfg_pet_clinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.cjvisions.sfg_pet_clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
